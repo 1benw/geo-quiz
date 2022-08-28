13904ff412292a8177bef9e72a1635763d893e27
Ben W <73312645+1benw@users.noreply.github.com>
2022-08-28 23:42:40 +0100

    Add loading overlay

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,31 @@
 import { useState } from 'react'
-import { Text } from '@mantine/core';
+import { Loader, LoadingOverlay, Text, Stack } from '@mantine/core';
 
 import { StartMenu, WaitingScreen } from './components';
 
 import { useGameStore } from './hooks';
 
 function App() {
+  const loading = useGameStore(state => state.loading);
+  const loadingText = useGameStore(state => state.loadingText);
+
   const inGame = useGameStore(state => state.connected);
 
   return (
     <>
+      <LoadingOverlay
+        loader={
+          <Stack style={{ width: '50vw' }}>
+            <Loader style={{ margin: 'auto' }} size="xl" color="blue" variant="bars" />
+            {loadingText && <Text align="center" size="lg" weight={500} style={{ width: '100%' }}>
+              {loadingText}
+            </Text>}
+          </Stack>
+        }
+        visible={loading}
+        overlayBlur={3}
+        transitionDuration={500}
+      />
       {inGame && <WaitingScreen />}
       {!inGame && <StartMenu />}
     </>
